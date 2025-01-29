311905b367aeb91ba637bb92143f5f36b307c58a
orian <orian.eluz@live.biu.ac.il>
2025-01-29 14:04:31 +0200

    merged eith signup

---
+++ netflix-master/backend/utils/jwt.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET = process.env.JWT_SECRET || "oursecretKey12yevce943bv3498g3v@ece389!@#$%^&*()";
+
+const createJWT = (user) => {
+    const payload = {
+        id: user.id,
+        username: user.username,
+        role: user.role
+    };
+    const options = {
+        expiresIn: '1d'
+    };
+
+    const token = jwt.sign(payload, JWT_SECRET, options);
+    return token;
+};
+
+module.exports = createJWT;
